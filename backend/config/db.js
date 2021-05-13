8cdf1dd24573ef891aa7cb852f83297a89e8f6b8
demabio <demabio91@gmail.com>
2021-05-13 04:41:10 +0300

    changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,21 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    })
+    const conn = await mongoose.connect(
+      "mongodb+srv://Cecile91:Cecile91@cluster0.mftcb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      }
+    );
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.error(`Error: ${error.message}`.red.underline.bold)
-    process.exit(1)
+    console.error(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1);
   }
-}
+};
 
-export default connectDB
+export default connectDB;
